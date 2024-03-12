16c987fd94cdab865da728a63af998fada26fc9d
Achiya Haviv <ahiya.haviv@bewith.io>
2024-03-12 19:01:54 +0200

    center style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import {Board} from "./components/mines/board.tsx";
+import './App.css'
 
 function App() {
 
     return (
         <>
-            <div>
+            <div className='main'>
                 <Board baseArray={[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]} minesPerRow={3}/>
             </div>
         </>
